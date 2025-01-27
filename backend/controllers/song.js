37c156a3ae57657e832c54143473be5e90ddc30c
Dell.O <sherdellmallett@gmail.com>
2025-01-27 16:27:06 -0600

    Search function

--- backend/controllers/song.js
+++ backend/controllers/song.js
@@ -3,11 +3,16 @@ const Song = require('../models/song');
 module.exports = {
   create,
   index,
+  search,
 };
 
 async function index(req, res) {
-  const songs = await Song.find({}).populate('playlist');
-  res.json(songs);
+  try {
+    const songs = await Song.find({}).populate('playlist');
+    res.json(songs);
+  } catch (err) {
+    res.status(400).json({ message: 'Failed to fetch songs' });
+  }
 }
 
 async function create(req, res) {
@@ -16,5 +21,20 @@ async function create(req, res) {
     res.json(song);
   } catch (err) {
     res.status(400).json({ message: 'Failed to create song' });
+  }
+}
+
+async function search(req, res) {
+  try {
+    const query = req.query.q || ''; // Search query from the client
+    const songs = await Song.find({
+      $or: [
+        { title: { $regex: query, $options: 'i' } },
+        { artist: { $regex: query, $options: 'i' } },
+      ],
+    });
+    res.json(songs);
+  } catch (err) {
+    res.status(400).json({ message: 'Failed to search for songs' });
   }
 }
